a843ebab517f4854b378d3e7bd76d3b98956a16f
NatalyCortez <flyingchair_pe12@hotmail.com>
2018-01-18 16:40:14 -0500

    agrenagndo contenido a las carpetas

---
+++ js/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	var $item = $('.carousel .item'); 
+	var $wHeight = $(window).height();
+	$item.height($wHeight); 
+	$item.addClass('full-screen');
+	
+	$('.carousel img').each(function() {
+		var $src = $(this).attr('src');
+		$(this).parent().css({
+			'background-image' : 'url(' + $src + ')',
+		});
+		$(this).remove();
+	});
+});
+	
+	$(window).on('resize', function (){
+		$wHeight = $(window).height();
+		$item.height($wHeight);
+	});
+	
+	$('.carousel').carousel({
+		interval: 5000,
+		pause: "false"
+	});
+
