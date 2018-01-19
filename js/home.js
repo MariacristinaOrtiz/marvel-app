bd15b5992b027fecb4fdc70aab1a3ab07eba8d59
elenaJO <ele.lucero1993@gmail.com>
2018-01-18 21:59:55 -0500

    añadiendo evento modal

--- js/home.js
+++ js/home.js
@@ -37,16 +37,27 @@ $(document).ready(function() {
     if ($('#filtros').html() === 'Título <span class="caret"></span>') {
       for (var i = 0; i < data.length; i++) {
         if (data[i]['Title'].indexOf(buscar) !== -1) {
-          console.log(data[i]);
+          // console.log(i);
           var appen = '<div class="row">' +
                        '<div class="col-xs-12 center">' +
-                         '<img src="_img_peli_" class = "img-pelis" data-toggle="modal" data-target="#click-pelicula" alt="">' +
+                         '<img src="_img_peli_" class = "img-pelis" alt="_num_"  data-toggle="modal" data-target="#click-pelicula">' +
                       '</div>' +
                     '</div>';
-          var appenReplace = appen.replace('_nombre_pelicula_', data[i]['Title']).replace('_img_peli_',data[i]['Poster']);
+          var appenReplace = appen.replace('_nombre_pelicula_', data[i]['Title']).replace('_img_peli_',data[i]['Poster']).replace('_num_', i);
           containerPelis.append(appenReplace);          
         }
       }
+
+      $('.img-pelis').each(function(index) {
+        $('.img-pelis').eq(index).click(function() {
+          var altAtribute = $('.img-pelis').eq(index).attr('alt');
+          $('#titulo-peli').text(data[altAtribute]['Title']);
+          $('#ano').text('Año : ' + data[altAtribute]['Year']);
+          $('#duracion').text('Duración : ' + data[altAtribute]['Runtime']);
+          $('#director').text('Director : ' + data[altAtribute]['Director']);
+          $('#actor').text('Actores : ' + data[altAtribute]['Actors']);
+        });
+      });
     }
   }
 });
