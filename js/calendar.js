7514449915aa3532b43356cfb9714d5fba8d7f05
Azur_Azabache <tharuka_20@hotmail.com>
2018-01-19 11:44:14 -0500

    update calendar-news

--- js/calendar.js
+++ js/calendar.js
@@ -1,18 +1,57 @@
 $(document).ready(function() {
   moment.locale('es');
+  var calHeight = 500;
   $('#calendar').fullCalendar({
+    header: {
+               left: 'prev,next today',
+               center: 'title',
+               right: 'month,basicWeek,basicDay'
+           },
     editable: true,
+
     weekMode: 'liquid',
     url:'#',
+    height:calHeight,
+    contentHeight:calHeight,
     events: [
                   {
-                      title: 'Pam Birthday',
-                      start: '2018- 01-12',
-                      allDay: true
+                      title: 'Black Panther',
+                      start: '2018-02-16',
+                      allDay: true,
+                      url: 'https://www.youtube.com/watch?v=fsT5SyBLlIg'
                   },
-
+                  {
+                      title: 'Jessica Jones Netflix 2°Season',
+                      start: '2018-03-08',
+                      allDay: true,
+                      url: 'https://www.youtube.com/watch?v=Wq7-ANZ_0e0'
+                  },
+                  {
+                      title: 'Avengers: Infinity War',
+                      start: '2018-05-04',
+                      allDay: true,
+                      url: 'https://www.youtube.com/watch?v=6ZfuNTqbHE8'
+                  },
+                  {
+                      title: 'Ant-Man annd the Wasp',
+                      start: '2018-07-06',
+                      allDay: true,
+                      url: 'https://www.youtube.com/watch?v=I3WHi5J1FUg'
+                  },
+                  {
+                      title: 'Venom',
+                      start: '2018-10-05',
+                      allDay: true,
+                      url: 'https://www.youtube.com/watch?v=8P0aaIOnYiE'
+                  },
+                  {
+                      title: 'Captain Marvel',
+                      start: '2019-03-08',
+                      allDay: true,
+                      url: 'https://www.youtube.com/watch?v=lokJkv32EUA'
+                  },
               ]
-});
 
+});
 
 });
