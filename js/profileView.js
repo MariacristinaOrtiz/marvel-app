28a926d2507ed954586bf9684a5d2bac1ca9671a
MariacristinaOrtiz <mari_d17@hotmail.com>
2018-01-22 17:47:39 -0500

    Añade modificaciones js de perfil para guardar datos de firebase de la lista de peliculas, hipervinvulos para tener un flujo

--- js/profileView.js
+++ js/profileView.js
@@ -12,13 +12,13 @@ $(document).ready(function() {
     $('#email-home h6:first-child').text(user.email);
     $('#img-profile').attr('src', user.foto);
   });
-  // AÑADE listaUno  a la vista PERFIL de Max
-  firebase.database().ref('usuarios/shCJTaLhHQWpO9PDXKqqeqc5ZkC3/listaUno' /* + localStorage.userUid '/' + localStorage.user.listaUno*/).on('value', function(snapshot) {
-    var listaUno = snapshot.val();
+  // AÑADE lista2  a la vista PERFIL de Max
+  firebase.database().ref('usuarios/' + localStorage.userUid + '/lista/2').on('value', function(snapshot) {
+    var lista2 = snapshot.val();
     console.log(snapshot);
-    console.log(listaUno);
+    console.log(lista2);
     // AÑADIR LISTA
-    $('#content-list').append('<div  id = "list1" class="list container-fluid"><div class = "row"><p class="col-xs-6 title-list">' + listaUno.title + '  (' + listaUno.year + ')' + '</p><p class="col-xs-6 text-right"><button id = "btn-remove1" class="btn btn-eliminar btn-xs"><span class="glyphicon glyphicon-trash"aria-hidden="true"></span></button><button type="button" id = "btn-estado1" class="btn btn-primary btn-color btn-xs">' + 'Pendiente' + '</button></p></div><div class = "row"><figure class="col-xs-4"><img width = "105px" height = "90px" class= "img-responsive" src="' + listaUno.poster + '" alt=""></figure><p class="col-xs-8 plot">' + listaUno.plot + '</p></div></div>');
+    $('#content-list').append('<div  id = "list1" class="list container-fluid"><div class = "row"><p class="col-xs-6 title-list">' + lista2.title + '  (' + lista2.year + ')' + '</p><p class="col-xs-6 text-right"><button id = "btn-remove1" class="btn btn-eliminar btn-xs"><span class="glyphicon glyphicon-trash"aria-hidden="true"></span></button><button type="button" id = "btn-estado1" class="btn btn-primary btn-color btn-xs">' + 'Pendiente' + '</button></p></div><div class = "row"><figure class="col-xs-4"><img width = "105px" height = "90px" class= "img-responsive" src="' + lista2.poster + '" alt=""></figure><p class="col-xs-8 plot">' + lista2.plot + '</p></div></div>');
     // CAmbiar el botón de visto a pendiente y viceversa
     console.log($('#btn-estado1'));
     var $buttonEstado = $('#btn-estado1');
@@ -28,20 +28,20 @@ $(document).ready(function() {
         $buttonEstado.text('Visto');
         console.log($buttonEstado.text());
         // Guardar en firebase
-        firebase.database().ref('usuarios/shCJTaLhHQWpO9PDXKqqeqc5ZkC3/listaUno' /* + localStorage.userUid '/' + localStorage.user.listaUno*/).update({
+        firebase.database().ref('usuarios/' + localStorage.userUid + '/lista/2').update({
           Estado: $buttonEstado.text(),
         });
       } else {
         $buttonEstado.text('Pendiente');
-        firebase.database().ref('usuarios/shCJTaLhHQWpO9PDXKqqeqc5ZkC3/listaUno' /* + localStorage.userUid '/' + localStorage.user.listaUno*/).update({
+        firebase.database().ref('usuarios/' + localStorage.userUid + '/lista/2').update({
           Estado: $buttonEstado.text(),
         });
       }
     });
     // Extraer la información del estado del buttonEstado
-    firebase.database().ref('usuarios/shCJTaLhHQWpO9PDXKqqeqc5ZkC3/listaUno' /* + localStorage.userUid '/' + localStorage.user.listaUno*/).on('value', function(snapshot) {
+    firebase.database().ref('usuarios/' + localStorage.userUid + '/lista/2').on('value', function(snapshot) {
       var listaUno = snapshot.val();
-      $buttonEstado.text(listaUno.Estado);
+      $buttonEstado.text(lista2.Estado);
     });
     // Eliminar
     $buttonRemove.on('click', function() {
@@ -49,13 +49,13 @@ $(document).ready(function() {
       // firebase.database().ref('usuarios/shCJTaLhHQWpO9PDXKqqeqc5ZkC3/listaUno' /* + localStorage.userUid '/' + localStorage.user.listaUno*/).remove();
     });
   });
-  // AÑADE listaDos  a la vista PERFIL de Max
-  firebase.database().ref('usuarios/shCJTaLhHQWpO9PDXKqqeqc5ZkC3/listaDos' /* + localStorage.userUid '/' + localStorage.user.listaUno*/).on('value', function(snapshot) {
-    var listaDos = snapshot.val();
+  // AÑADE lista4  a la vista PERFIL de Max
+  firebase.database().ref('usuarios/' + localStorage.userUid + '/lista/4' /* + localStorage.userUid '/' + localStorage.user.listaUno*/).on('value', function(snapshot) {
+    var lista4 = snapshot.val();
     console.log(snapshot);
-    console.log(listaDos);
+    console.log(lista4);
     // AÑADIR LISTA
-    $('#content-list').append('<div id ="list" class="list container-fluid"><div class = "row"><p class="col-xs-6 title-list">' + listaDos.title + '  (' + listaDos.year + ')' + '</p><p class="col-xs-6 text-right"><button id = "btn-remove" class="btn btn-eliminar btn-xs"><span class="glyphicon glyphicon-trash"aria-hidden="true"></span></button><button type="button"id = "btn-estado2" class="btn btn-primary btn-color btn-xs">' + 'Pendiente' + '</button></p></div><div class = "row"><figure class="col-xs-4"><img  class= "img-responsive" src="' + listaDos.poster + '" alt=""></figure><p class="col-xs-8 plot">' + listaDos.plot + '</p></div></div>');
+    $('#content-list').append('<div id ="list" class="list container-fluid"><div class = "row"><p class="col-xs-6 title-list">' + lista4.title + '  (' + lista4.year + ')' + '</p><p class="col-xs-6 text-right"><button id = "btn-remove" class="btn btn-eliminar btn-xs"><span class="glyphicon glyphicon-trash"aria-hidden="true"></span></button><button type="button"id = "btn-estado2" class="btn btn-primary btn-color btn-xs">' + 'Pendiente' + '</button></p></div><div class = "row"><figure class="col-xs-4"><img  class= "img-responsive" src="' + lista4.poster + '" alt=""></figure><p class="col-xs-8 plot">' + lista4.plot + '</p></div></div>');
     // CAmbiar el botón de visto a pendiente y viceversa
     var $buttonEstado = $('#btn-estado2');
     var $buttonRemove = $('#btn-remove');
@@ -64,21 +64,21 @@ $(document).ready(function() {
         $buttonEstado.text('Visto');
         console.log($buttonEstado.text());
         // Guardar en firebase
-        firebase.database().ref('usuarios/shCJTaLhHQWpO9PDXKqqeqc5ZkC3/listaDos' /* + localStorage.userUid '/' + localStorage.user.listaUno*/).update({
+        firebase.database().ref('usuarios/' + localStorage.userUid + '/lista/4'/* + localStorage.userUid '/' + localStorage.user.listaUno*/).update({
           Estado: $buttonEstado.text(),
         });
       } else {
         $buttonEstado.text('Pendiente');
         $buttonEstado.addClass('Pendiente');
-        firebase.database().ref('usuarios/shCJTaLhHQWpO9PDXKqqeqc5ZkC3/listaDos' /* + localStorage.userUid*/).update({
+        firebase.database().ref('usuarios/' + localStorage.userUid + '/lista/4' /* + localStorage.userUid*/).update({
           Estado: $buttonEstado.text(),
         });
       }
     });
     // Extraer la información del estado del buttonEstado
-    firebase.database().ref('usuarios/shCJTaLhHQWpO9PDXKqqeqc5ZkC3/listaDos' /* + localStorage.userUid '/' */).on('value', function(snapshot) {
+    firebase.database().ref('usuarios/' + localStorage.userUid + '/lista/4' /* + localStorage.userUid '/' */).on('value', function(snapshot) {
       var listaDos = snapshot.val();
-      $buttonEstado.text(listaDos.Estado);
+      $buttonEstado.text(lista4.Estado);
     });
     // Eliminar
     $buttonRemove.on('click', function() {
@@ -88,12 +88,12 @@ $(document).ready(function() {
   // ------ Fin de agregar listaDos------------
   // ListaTres
   // AÑADE listaUno  a la vista PERFIL de Max
-  firebase.database().ref('usuarios/shCJTaLhHQWpO9PDXKqqeqc5ZkC3/lista/1' /* + localStorage.userUid '/' + localStorage.user.listaUno*/).on('value', function(snapshot) {
+  firebase.database().ref('usuarios/' + localStorage.userUid + '/lista/6' /* + localStorage.userUid '/' + localStorage.user.listaUno*/).on('value', function(snapshot) {
     var lista1 = snapshot.val();
     console.log(snapshot);
-    console.log(lista1);
+    console.log(lista6);
     // AÑADIR LISTA
-    $('#content-list').append('<div  id = "list1" class="list container-fluid"><div class = "row"><p class="col-xs-6 title-list">' + lista1.title + '  (' + lista1.year + ')' + '</p><p class="col-xs-6 text-right"><button id = "btn-remove2" class="btn btn-eliminar btn-xs"><span class="glyphicon glyphicon-trash"aria-hidden="true"></span></button><button type="button" id = "btn-estado3" class="btn btn-primary btn-color btn-xs">' + 'Pendiente' + '</button></p></div><div class = "row"><figure class="col-xs-4"><img width = "105px" height = "90px" class= "img-responsive" src="' + lista1.poster + '" alt=""></figure><p class="col-xs-8 plot">' + lista1.plot + '</p></div></div>');
+    $('#content-list').append('<div  id = "list6" class="list container-fluid"><div class = "row"><p class="col-xs-6 title-list">' + lista6.title + '  (' + lista6.year + ')' + '</p><p class="col-xs-6 text-right"><button id = "btn-remove2" class="btn btn-eliminar btn-xs"><span class="glyphicon glyphicon-trash"aria-hidden="true"></span></button><button type="button" id = "btn-estado3" class="btn btn-primary btn-color btn-xs">' + 'Pendiente' + '</button></p></div><div class = "row"><figure class="col-xs-4"><img width = "105px" height = "90px" class= "img-responsive" src="' + lista6.poster + '" alt=""></figure><p class="col-xs-8 plot">' + lista6.plot + '</p></div></div>');
     // CAmbiar el botón de visto a pendiente y viceversa
     console.log($('#btn-estado3'));
     var $buttonEstado = $('#btn-estado3');
@@ -103,24 +103,24 @@ $(document).ready(function() {
         $buttonEstado.text('Visto');
         console.log($buttonEstado.text());
         // Guardar en firebase
-        firebase.database().ref('usuarios/shCJTaLhHQWpO9PDXKqqeqc5ZkC3/lista/1' /* + localStorage.userUid '/' + localStorage.user.listaUno*/).update({
+        firebase.database().ref('usuarios/' + localStorage.userUid + '/lista/6' /* + localStorage.userUid '/' + localStorage.user.listaUno*/).update({
           Estado: $buttonEstado.text(),
         });
       } else {
         $buttonEstado.text('Pendiente');
-        firebase.database().ref('usuarios/shCJTaLhHQWpO9PDXKqqeqc5ZkC3/lista/1' /* + localStorage.userUid '/' + localStorage.user.listaUno*/).update({
+        firebase.database().ref('usuarios/' + localStorage.userUid + '/lista/6' /* + localStorage.userUid '/' + localStorage.user.listaUno*/).update({
           Estado: $buttonEstado.text(),
         });
       }
     });
     // Extraer la información del estado del buttonEstado
-    firebase.database().ref('usuarios/shCJTaLhHQWpO9PDXKqqeqc5ZkC3/lista/1' /* + localStorage.userUid '/' + localStorage.user.listaUno*/).on('value', function(snapshot) {
+    firebase.database().ref('usuarios/' + localStorage.userUid + '/lista/6' /* + localStorage.userUid '/' + localStorage.user.listaUno*/).on('value', function(snapshot) {
       var lista1 = snapshot.val();
-      $buttonEstado.text(lista1.Estado);
+      $buttonEstado.text(lista6.Estado);
     });
     // Eliminar
     $buttonRemove.on('click', function() {
-      $('#list1').remove();
+      $('#list6').remove();
       // firebase.database().ref('usuarios/shCJTaLhHQWpO9PDXKqqeqc5ZkC3/listaUno' /* + localStorage.userUid '/' + localStorage.user.listaUno*/).remove();
     });
   });
@@ -140,14 +140,14 @@ $(document).ready(function() {
   $('#ratings').on('click', function() {
     $('#ratings-child').toggleClass('hidden');
     $('#ratings div:first-child').toggleClass('hidden');
-    firebase.database().ref('usuarios/shCJTaLhHQWpO9PDXKqqeqc5ZkC3/listaUno' /* + localStorage.userUid '/' + localStorage.user.listaUno*/).on('value', function(snapshot) {
-      var listaUno = snapshot.val();
-      $('#ratings-child').append('<div class ="container content-ratings"><figure class="col-xs-3"><img width="50px" height ="60px" class= "img-responsive" src="' + listaUno.poster + '" alt=""></figure><p class="col-xs-9 title-ratings "><strong>' + listaUno.title + '  (' + listaUno.year + ')' + '</strong><p   class="col-xs-9"><span class="glyphicon glyphicon-star" aria-hidden="true"></span><span>' + listaUno.ratings + '</span></p><p class=" coment plot col-xs-12">' + 'Hey!! Este es un regreso a la forma de Marvel, presentando un nuevo héroe que estoy feliz de ver de nuevo, oh!!' + '</p></div>');
+    firebase.database().ref('usuarios/' + localStorage.userUid + '/lista/2' /* + localStorage.userUid '/' + localStorage.user.listaUno*/).on('value', function(snapshot) {
+      var lista2 = snapshot.val();
+      $('#ratings-child').append('<div class ="container content-ratings"><figure class="col-xs-3"><img width="50px" height ="60px" class= "img-responsive" src="' + lista2.poster + '" alt=""></figure><p class="col-xs-9 title-ratings "><strong>' + lista2.title + '  (' + lista2.year + ')' + '</strong><p   class="col-xs-9"><span class="glyphicon glyphicon-star" aria-hidden="true"></span><span>' + lista2.ratings + '</span></p><p class=" coment plot col-xs-12">' + 'Hey! aquí irá la colección de tus comentarios y calificaciones, estamos trabajando para que puedas usar esta funcionalidad que te da Marvel fans :)!!' + '</p></div>');
     });
   });
   // Redireccion de vistas
     $('#usuario-nav').on('click', function(){
       window.location.href = '../index.html';
     })
-    
+
 });
